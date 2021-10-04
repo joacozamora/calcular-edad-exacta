520cb5cf7f46bdb57e753e6be00cb67519d57efd
joacozamora <joacozamoracab@gmail.com>
2021-10-04 12:26:50 -0300

    Primera version

---
+++ Calcular edad exacta.WinForm/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace Contados_de_dias_vividos.WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btEdadExacta = new System.Windows.Forms.Button();
+            this.lblAnios = new System.Windows.Forms.Label();
+            this.lblMes = new System.Windows.Forms.Label();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.lblTotalDias = new System.Windows.Forms.Label();
+            this.btTotalDias = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblCargando = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FECHA DE NACIMIENTO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(452, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FECHA ACTUAL";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(61, 98);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(455, 97);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 3;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // btEdadExacta
+            // 
+            this.btEdadExacta.Location = new System.Drawing.Point(155, 166);
+            this.btEdadExacta.Name = "btEdadExacta";
+            this.btEdadExacta.Size = new System.Drawing.Size(158, 23);
+            this.btEdadExacta.TabIndex = 4;
+            this.btEdadExacta.Text = "Calcular tu Edad Exacta";
+            this.btEdadExacta.UseVisualStyleBackColor = true;
+            this.btEdadExacta.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblAnios
+            // 
+            this.lblAnios.AutoSize = true;
+            this.lblAnios.Location = new System.Drawing.Point(245, 272);
+            this.lblAnios.Name = "lblAnios";
+            this.lblAnios.Size = new System.Drawing.Size(10, 13);
+            this.lblAnios.TabIndex = 5;
+            this.lblAnios.Text = "-";
+            // 
+            // lblMes
+            // 
+            this.lblMes.AutoSize = true;
+            this.lblMes.Location = new System.Drawing.Point(327, 272);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(10, 13);
+            this.lblMes.TabIndex = 6;
+            this.lblMes.Text = "-";
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Location = new System.Drawing.Point(454, 272);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(10, 13);
+            this.lblDias.TabIndex = 7;
+            this.lblDias.Text = "-";
+            // 
+            // lblTotalDias
+            // 
+            this.lblTotalDias.AutoSize = true;
+            this.lblTotalDias.Location = new System.Drawing.Point(343, 361);
+            this.lblTotalDias.Name = "lblTotalDias";
+            this.lblTotalDias.Size = new System.Drawing.Size(10, 13);
+            this.lblTotalDias.TabIndex = 8;
+            this.lblTotalDias.Text = "-";
+            // 
+            // btTotalDias
+            // 
+            this.btTotalDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTotalDias.Location = new System.Drawing.Point(381, 166);
+            this.btTotalDias.Name = "btTotalDias";
+            this.btTotalDias.Size = new System.Drawing.Size(158, 23);
+            this.btTotalDias.TabIndex = 9;
+            this.btTotalDias.Text = "Calcular Dias Vividos";
+            this.btTotalDias.UseVisualStyleBackColor = true;
+            this.btTotalDias.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(282, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Años";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(371, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Meses";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(488, 272);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Dias";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(242, 361);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Tienes un total de:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(378, 361);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "dias vividos.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(104, 272);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Tu edad exacta es de:";
+            // 
+            // lblCargando
+            // 
+            this.lblCargando.AutoSize = true;
+            this.lblCargando.Location = new System.Drawing.Point(296, 223);
+            this.lblCargando.Name = "lblCargando";
+            this.lblCargando.Size = new System.Drawing.Size(0, 13);
+            this.lblCargando.TabIndex = 16;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Window;
+            this.progressBar1.Location = new System.Drawing.Point(285, 214);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(124, 34);
+            this.progressBar1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCargando);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btTotalDias);
+            this.Controls.Add(this.lblTotalDias);
+            this.Controls.Add(this.lblDias);
+            this.Controls.Add(this.lblMes);
+            this.Controls.Add(this.lblAnios);
+            this.Controls.Add(this.btEdadExacta);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button btEdadExacta;
+        private System.Windows.Forms.Label lblAnios;
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.Label lblDias;
+        private System.Windows.Forms.Label lblTotalDias;
+        private System.Windows.Forms.Button btTotalDias;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblCargando;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Calcular edad exacta.WinForm/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Contados_de_dias_vividos.WinForm
+{
+    public partial class Form1 : Form
+    {
+        int Anio_Nacimiento;
+        int Mes_Nacimiento;
+        int Dia_Nacimiento;
+
+        int Anio_Actual;
+        int Mes_Actual;
+        int Dia_Actual;
+
+        int Anios_Vividos;
+        int Meses_Vividos;
+        int Dias_Vividos;
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap img = new Bitmap(Application.StartupPath + @"\img\fondo.jpg");
+            this.BackgroundImage = img;
+        }
+        
+        public void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            Anio_Nacimiento = dateTimePicker1.Value.Year;
+            Mes_Nacimiento = dateTimePicker1.Value.Month;
+            Dia_Nacimiento = dateTimePicker1.Value.Day;
+            MessageBox.Show(System.Convert.ToString("Fecha de nacimiento seleccionada correctamente"));
+        }
+
+        public void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            
+            Anio_Actual = dateTimePicker2.Value.Year;
+            Mes_Actual = dateTimePicker2.Value.Month;
+            Dia_Actual = dateTimePicker2.Value.Day;
+            MessageBox.Show(System.Convert.ToString("Fecha actual seleccionada correctamente"));
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            
+            while (i != 51)
+            {
+
+
+                
+                string cadena = new string('|', i);
+                lblCargando.Text = cadena;
+                Thread.Sleep(5);
+                this.Refresh();
+                i = i + 1;
+                    
+            }
+
+            bool opcion0 = false;
+            if (Anio_Actual == 0)
+            {
+                MessageBox.Show("Debes remarcar aunque sea una vez la fecha actual o dara error en los numeros.");
+                
+                opcion0 = true;
+            }
+
+            if (opcion0 == true)
+            {
+                Meses_Vividos = -1;
+                Dias_Vividos = -1;
+                Anios_Vividos = -1;
+                
+            }
+            
+
+            bool opcion1 = false;
+            if (Dia_Actual < Dia_Nacimiento & Mes_Actual < Mes_Nacimiento & opcion0== false)
+            {
+                opcion1 = true;
+            }
+            
+            bool opcion2 = false;
+            if(Dia_Actual < Dia_Nacimiento & Mes_Actual > Mes_Nacimiento)
+            {
+                opcion2 = true;
+            }
+            bool opcion3 = false;
+            if (Dia_Actual > Dia_Nacimiento & Mes_Actual < Mes_Nacimiento)
+            {
+                opcion3 = true;
+            }
+            bool opcion4 = false;
+            if (Dia_Actual >= Dia_Nacimiento & Mes_Actual >= Mes_Nacimiento)
+            {
+                opcion4 = true;
+            }
+
+            
+
+            if (opcion1 == true)
+            {
+                Meses_Vividos = (12 - Mes_Nacimiento) + Mes_Actual - 1;
+                Dias_Vividos = (30 - Dia_Nacimiento) + Dia_Actual;
+                Anios_Vividos = Anio_Actual - Anio_Nacimiento - 1;
+            }
+
+            else if (opcion2 == true)
+            {
+                Meses_Vividos = (Mes_Actual - Mes_Nacimiento) - 1;
+                Dias_Vividos = (30 - Dia_Nacimiento) + Dia_Actual;
+                Anios_Vividos = Anio_Actual - Anio_Nacimiento;
+
+
+            }
+
+            else if (opcion3 == true)
+            {
+                Meses_Vividos = (12 - Mes_Nacimiento) + Mes_Actual;
+                Dias_Vividos = Dia_Actual - Dia_Nacimiento;
+                Anios_Vividos = Anio_Actual - Anio_Nacimiento - 1;
+            }
+
+            else if (opcion4 == true)
+            {
+                Meses_Vividos = Mes_Actual - Mes_Nacimiento;
+                Dias_Vividos = Dia_Actual - Dia_Nacimiento;
+                Anios_Vividos = Anio_Actual - Anio_Nacimiento;
+            }
+
+            
+
+            
+            lblAnios.Text = System.Convert.ToString(Anios_Vividos);
+            lblMes.Text = System.Convert.ToString(Meses_Vividos);
+            lblDias.Text = System.Convert.ToString(Dias_Vividos);
+
+            
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            int Cantidad_Dias_Meses = Meses_Vividos * 30;
+            int Cantidad_Dias_Anios = Anios_Vividos * 365;
+            int Cantidad_Dias_Total = Dias_Vividos + Cantidad_Dias_Anios + Cantidad_Dias_Meses;
+            lblTotalDias.Text = System.Convert.ToString(Cantidad_Dias_Total);
+        }
+    }
+}
